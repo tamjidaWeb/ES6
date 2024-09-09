55caa602d10332b5af5f1857bd51f3e4a49c0289
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-09 12:06:46 +0600

    commited

--- chain.js
+++ chain.js
@@ -1,3 +1,36 @@
 //data access
 const data = [{id:1, name: 'abul', address: 'kochukhet'}];
-console.log(data[2])
+console.log(data[0].address);
+
+const products = {
+    count:5000,
+    data:[
+        {id:1, name:'laptop', price : 65000},
+        {id:2, name:'laptop2', price: 120000},
+    ]
+}
+console.log(products.data[1].price);
+
+const user = {
+    id:5001,
+    name: ' raj',
+    address:{
+        street:{
+            first: '34/c uttor side',
+            second: ' badda',
+            third:'no bari',
+
+        },
+        city:'dhaka',
+    }
+}
+const user2 = {
+    id: 5002,
+    name:'pori bibir majar',
+    address:{
+        city:'chittagong',
+        country:'bangladesh',
+    }
+}
+console.log(user2.address.street?.second)
+console.log(user.address.street?.second)

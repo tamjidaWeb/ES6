c4e6dc2616878f5f34538c81a96cc221199fdcce
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-10 17:19:24 +0600

    commited

--- dot.js
+++ dot.js
@@ -1,12 +1,16 @@
-const person= {
-    name:'kodom ali',
-    job:'badam khai',
-    3:'third',
-    'add-dress':
-}
-const prop = 'job'
-console.log(person.job);
-console.log(person['job'])
-console.log(person[prop])
-console.log(person['3'])
-console.log(p)
+// const person= {
+//     name:'kodom ali',
+//     job:'badam khai',
+//     3:'third',
+//     'add-dress':
+// // }
+// const prop = 'job'
+// console.log(person.job);
+// console.log(person['job'])
+// console.log(person[prop])
+// console.log(person['3'])
+// console.log(p)
+
+let array = [1, 2, 3, 4, 5];
+array.length = 0;
+console.log(array);

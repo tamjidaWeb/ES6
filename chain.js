f084c0d546a8e8b5c1dae9c7525aae6e185982ed
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-11 21:08:28 +0600

    commited

--- chain.js
+++ chain.js
@@ -1,36 +1,75 @@
+// //data access
+// const data = [{id:1, name: 'abul', address: 'kochukhet'}];
+// console.log(data[0].address);
+
+// const products = {
+//     count:5000,
+//     data:[
+//         {id:1, name:'laptop', price : 65000},
+//         {id:2, name:'laptop2', price: 120000},
+//     ]
+// }
+// console.log(products.data[1].price);
+
+// const user = {
+//     id:5001,
+//     name: ' raj',
+//     address:{
+//         street:{
+//             first: '34/c uttor side',
+//             second: ' badda',
+//             third:'no bari',
+
+//         },
+//         city:'dhaka',
+//     }
+// }
+// const user2 = {
+//     id: 5002,
+//     name:'pori bibir majar',
+//     address:{
+//         city:'chittagong',
+//         country:'bangladesh',
+//     }
+// }
+// console.log(user2.address.street?.second)
+// console.log(user.address.street?.second)
+
+
 //data access
-const data = [{id:1, name: 'abul', address: 'kochukhet'}];
-console.log(data[0].address);
+
+const data = [{id:1, name:'abul', address:'kochu khet'}]
+// console.log(data[0].address)
 
 const products = {
-    count:5000,
+    count: 500,
     data:[
-        {id:1, name:'laptop', price : 65000},
-        {id:2, name:'laptop2', price: 120000},
+        {id:1, name:'lenevo laptop', price:65000},
+        {id:2, name:'mac', price:120000}
     ]
 }
-console.log(products.data[1].price);
+
+// console.log(products.data[1].price);
 
 const user = {
-    id:5001,
-    name: ' raj',
+    id: 5001,
+    name:'shoriful raj',
     address:{
         street:{
-            first: '34/c uttor side',
-            second: ' badda',
-            third:'no bari',
-
+            first:' 68/c, asad avenue',
+            second: 'dhanmondi'
         },
-        city:'dhaka',
+        city:'dhaka'
     }
 }
+
 const user2 = {
-    id: 5002,
-    name:'pori bibir majar',
+    id:5002,
+    name:'pori biwir majar',
     address:{
-        city:'chittagong',
-        country:'bangladesh',
+        city: 'cittagong',
+        country:'bangladesh'
     }
 }
-console.log(user2.address.street?.second)
-console.log(user.address.street?.second)
+console.log(user2.address.street?.country)
+console.log(user2.address.country);
---
+++ recape.js
@@ -0,0 +1,27 @@
+//
+//1. vat (let const)-block scope
+//2. default parameter
+//3. template string
+//4.arrow function
+//5.destructering and spread
+//6. Object.keys, Object.values, Object.entries
+//7. for of used in array and string
+//8. for in loop used in object
+
+
+// const a= 56;
+// const numbers = [4,5,3,9,7,6];
+// const person={name:'sakib'};
+// const message = `hi, ${person.name} has a: ${a} access to ${numbers[2]}`;
+
+
+// const {age,z, ...others} = {x:2, y:5, x:3, name: 'ovijeet', age: 55};
+
+// const [first,second, ...remaining] =['sunu', 'munu', 'kunu'];
+
+
+// const squre = x => x*x;
+// const sum = (a,b)=> a+b;
+// if(true){
+
+// }
